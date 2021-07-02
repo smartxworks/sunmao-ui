e087d210d1ddf49cb7aead742f4b20a93651341c
Yanzhen Yu <yanzhen@smartx.com>
2021-07-02 18:23:20 +0800

    init runtime with playground

---
+++ packages/runtime/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
