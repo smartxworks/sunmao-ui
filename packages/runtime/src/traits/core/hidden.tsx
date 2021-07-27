0f7825613a052b38ffbacbaa158f6d6afd5cdbfe
Yanzhen Yu <yanzhen@smartx.com>
2021-07-27 10:44:19 +0800

    close #18 impl hidden trait

---
+++ packages/runtime/src/traits/core/hidden.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { createTrait } from "@meta-ui/core";
+import { Static, Type } from "@sinclair/typebox";
+import { TraitImplementation } from "../../registry";
+import { useExpression } from "../../store";
+
+type HiddenProps = {
+  hidden: Static<typeof HiddenPropertySchema>;
+};
+
+const Hidden: React.FC<HiddenProps> = ({ hidden: _hidden, children }) => {
+  const hidden = useExpression(_hidden.toString());
+  if (hidden) {
+    return null;
+  }
+  return <>{children}</>;
+};
+
+const useHiddenState: TraitImplementation<HiddenProps> = ({ hidden }) => {
+  return {
+    props: null,
+    component: (props) => <Hidden {...props} hidden={hidden} />,
+  };
+};
+
+const HiddenPropertySchema = Type.Union([Type.Boolean(), Type.String()]);
+
+export default {
+  ...createTrait({
+    version: "core/v1",
+    metadata: {
+      name: "hidden",
+      description: "render component with condition",
+    },
+    spec: {
+      properties: [
+        {
+          name: "hidden",
+          ...HiddenPropertySchema,
+        },
+      ],
+      state: {},
+      methods: [],
+    },
+  }),
+  impl: useHiddenState,
+};
