9027e65bd02a97019620ae8ee87e3ba1d98b3ac1
chenenpei <enpei.chen@smartx.com>
2021-08-09 18:04:33 +0800

    implement chakra ui stack #5

---
+++ packages/runtime/src/components/chakra-ui/VStack.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { createComponent } from "@meta-ui/core";
+import { Static } from "@sinclair/typebox";
+import { VStack as BaseVStack } from "@chakra-ui/react";
+import { ComponentImplementation } from "../../registry";
+import Slot from "../_internal/Slot";
+import {
+  DirectionSchema,
+  FlexWrapSchema,
+  AlignItemsSchema,
+  JustifyContentSchema,
+  SpacingSchema,
+} from "./Stack";
+
+const VStack: ComponentImplementation<{
+  direction?: Static<typeof DirectionSchema>;
+  wrap?: Static<typeof FlexWrapSchema>;
+  align?: Static<typeof AlignItemsSchema>;
+  justify?: Static<typeof JustifyContentSchema>;
+  spacing?: Static<typeof SpacingSchema>;
+}> = ({ direction, wrap, align, justify, spacing, slotsMap }) => {
+  return (
+    <BaseVStack {...{ direction, wrap, align, justify, spacing }}>
+      <Slot slotsMap={slotsMap} slot="content" />
+    </BaseVStack>
+  );
+};
+
+export default {
+  ...createComponent({
+    version: "chakra_ui/v1",
+    metadata: {
+      name: "vstack",
+      description: "chakra-ui vstack",
+    },
+    spec: {
+      properties: [
+        {
+          name: "diection",
+          ...DirectionSchema,
+        },
+        {
+          name: "wrap",
+          ...FlexWrapSchema,
+        },
+        {
+          name: "align",
+          ...AlignItemsSchema,
+        },
+        {
+          name: "justify",
+          ...JustifyContentSchema,
+        },
+        {
+          name: "spacing",
+          ...SpacingSchema,
+        },
+      ],
+      acceptTraits: [],
+      state: {},
+      methods: [],
+    },
+  }),
+  impl: VStack,
+};
