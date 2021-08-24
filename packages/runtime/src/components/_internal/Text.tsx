cf02305f3edf691a83bc81655ab3c958c31417aa
Bowen Tan <bowen.tan@smartx.com>
2021-08-24 17:40:27 +0800

    input support style property

--- packages/runtime/src/components/_internal/Text.tsx
+++ packages/runtime/src/components/_internal/Text.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Static, Type } from '@sinclair/typebox';
+import { ComponentImplementation } from '../../registry';
 
 export const TextPropertySchema = Type.Object({
   raw: Type.String(),
@@ -16,11 +17,11 @@ export type TextProps = {
   value: Static<typeof TextPropertySchema>;
 };
 
-const Text: React.FC<TextProps> = ({ value }) => {
+const Text: ComponentImplementation<TextProps> = ({ value, style }) => {
   if (value.format === 'md') {
     return <ReactMarkdown>{value.raw}</ReactMarkdown>;
   }
-  return <>{value.raw}</>;
+  return <span style={style}>{value.raw}</span>;
 };
 
 export default Text;
