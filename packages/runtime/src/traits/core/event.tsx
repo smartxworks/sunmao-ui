bdfe076e6556f95eb3ea9a81e9435add25e2c7f4
Bowen Tan <bowen.tan@smartx.com>
2021-08-25 15:02:06 +0800

    refactor event trait

--- packages/runtime/src/traits/core/event.tsx
+++ packages/runtime/src/traits/core/event.tsx
@@ -1,4 +1,3 @@
-import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { createTrait } from '@meta-ui/core';
 import { Static, Type } from '@sinclair/typebox';
 import { debounce, throttle, delay } from 'lodash';
@@ -8,64 +7,54 @@ import { apiService } from '../../api-service';
 const useEventTrait: TraitImplementation<{
   events: Static<typeof EventsPropertySchema>;
 }> = ({ events }) => {
-  const handlerMap = useRef<Record<string, Array<(parameters?: any) => void>>>(
-    {}
-  );
-  const eventHandler = useCallback((s: { name: string; parameters?: any }) => {
-    if (!handlerMap.current[s.name]) {
+  const handlerMap: Record<string, Array<(parameters?: any) => void>> = {};
+  const eventHandler = (s: { name: string; parameters?: any }) => {
+    if (!handlerMap[s.name]) {
       // maybe log?
       return;
     }
-    handlerMap.current[s.name].forEach(fn => fn(s.parameters));
-  }, []);
+    handlerMap[s.name].forEach(fn => fn(s.parameters));
+  };
 
-  useEffect(() => {
-    // tear down previous handlers
-    handlerMap.current = {};
-    // setup current handlers
-    for (const event of events) {
-      const handler = () => {
-        let disabled = false;
-        if (typeof event.disabled === 'boolean') {
-          disabled = event.disabled;
-        }
-        if (disabled) {
-          return;
-        }
-        apiService.send('uiMethod', {
-          componentId: event.componentId,
-          name: event.method.name,
-          parameters: event.method.parameters,
-        });
-      };
-      if (!handlerMap.current[event.event]) {
-        handlerMap.current[event.event] = [];
+  // setup current handlers
+  for (const event of events) {
+    const handler = () => {
+      let disabled = false;
+      if (typeof event.disabled === 'boolean') {
+        disabled = event.disabled;
       }
-      handlerMap.current[event.event].push(
-        event.wait.type === 'debounce'
-          ? debounce(handler, event.wait.time)
-          : event.wait.type === 'throttle'
-          ? throttle(handler, event.wait.time)
-          : event.wait.type === 'delay'
-          ? () => delay(handler, event.wait.time)
-          : handler
-      );
+      if (disabled) {
+        return;
+      }
+      apiService.send('uiMethod', {
+        componentId: event.componentId,
+        name: event.method.name,
+        parameters: event.method.parameters,
+      });
+    };
+    if (!handlerMap[event.event]) {
+      handlerMap[event.event] = [];
     }
-  }, [events]);
+    handlerMap[event.event].push(
+      event.wait.type === 'debounce'
+        ? debounce(handler, event.wait.time)
+        : event.wait.type === 'throttle'
+        ? throttle(handler, event.wait.time)
+        : event.wait.type === 'delay'
+        ? () => delay(handler, event.wait.time)
+        : handler
+    );
+  }
 
-  const hub = useMemo(() => {
-    return {
+  return {
+    props: {
       // HARDCODE
       onClick() {
         eventHandler({
           name: 'click',
         });
       },
-    };
-  }, []);
-
-  return {
-    props: hub,
+    },
   };
 };
 
