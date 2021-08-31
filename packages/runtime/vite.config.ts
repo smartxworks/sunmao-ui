1e0ec7a2a40a9d570561ae04afcd0a2a2986a985
Bowen Tan <bowen.tan@smartx.com>
2021-08-31 11:08:11 +0800

    add linaria

--- packages/runtime/vite.config.ts
+++ packages/runtime/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import VitePluginLinaria from 'vite-plugin-linaria';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), VitePluginLinaria()],
   define: {
     // https://github.com/satya164/react-simple-code-editor/issues/86
-    global: "globalThis",
+    global: 'globalThis',
   },
 });
