84748086e6997a9f76f5ee33ebd39724a40e2bbf
Bowen Tan <bowen.tan@smartx.com>
2021-09-07 11:15:57 +0800

    add parseType util file

---
+++ packages/runtime/src/utils/parseType.ts
@@ -0,0 +1,16 @@
+// parse component Type
+export function parseType(v: string) {
+  const TYPE_REG = /^([a-zA-Z0-9_\d]+\/[a-zA-Z0-9_\d]+)\/([a-zA-Z0-9_\d]+)$/;
+  function isValidType(v: string): boolean {
+    return TYPE_REG.test(v);
+  }
+  if (!isValidType(v)) {
+    throw new Error(`Invalid type string: "${v}"`);
+  }
+
+  const [, version, name] = v.match(TYPE_REG)!;
+  return {
+    version,
+    name,
+  };
+}
