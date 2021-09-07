6bba9eb62027478b2ac15fb7f3b4fc5a792e843c
Bowen Tan <bowen.tan@smartx.com>
2021-09-07 17:25:17 +0800

    fix jest test

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  transform: {
-    ".ts": "ts-jest",
-  },
-  globals: {
-    "ts-jest": {
-      diagnostics: false,
-    },
-  },
   moduleFileExtensions: ["ts", "js"],
   testMatch: ["<rootDir>/__tests__/**/**.spec.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/lib/", "<rootDir>/lib/"],
