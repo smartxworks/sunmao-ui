05be1c755bd8e5677e2a017ca2d9a4982f6ece5b
Shengjie Yang <shengjie.yang@smartx.com>
2021-09-08 20:13:15 +0800

    feat: impl dialog component

---
+++ packages/runtime/src/components/chakra-ui/Dialog.tsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState, useRef } from "react";
+import { createComponent } from "@meta-ui/core";
+import { ComponentImplementation } from "../../registry";
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import { Static, Type } from "@sinclair/typebox";
+import { ColorSchemePropertySchema } from "./Button";
+import Slot from "../_internal/Slot";
+
+const TitlePropertySchema = Type.Optional(Type.String());
+
+const HandleButtonPropertySchema = Type.Object({
+  text: Type.Optional(Type.String()),
+  colorScheme: ColorSchemePropertySchema,
+});
+
+const Dialog: ComponentImplementation<{
+  title?: Static<typeof TitlePropertySchema>;
+  confirmButton?: Static<typeof HandleButtonPropertySchema>;
+  cancelButton?: Static<typeof HandleButtonPropertySchema>;
+}> = ({
+  slotsMap,
+  subscribeMethods,
+  callbackMap: callbacks,
+  title: customerTitle,
+  confirmButton = {
+    text: "confirm",
+    colorScheme: "red",
+  },
+  cancelButton = {
+    text: "cancel",
+    colorScheme: "blue",
+  },
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState(customerTitle || "");
+  const cancelRef = useRef(null);
+
+  useEffect(() => {
+    subscribeMethods({
+      openDialog({ title }) {
+        setIsOpen(true);
+        setTitle(title);
+      },
+      confirmDialog() {
+        setIsOpen(false);
+      },
+      cancelDialog() {
+        setIsOpen(false);
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={() => setIsOpen(false)}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader>{title}</AlertDialogHeader>
+
+            <AlertDialogBody>
+              <Slot slotsMap={slotsMap} slot="content" />
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button
+                ref={cancelRef}
+                colorScheme={cancelButton.colorScheme}
+                onClick={callbacks?.cancelDialog}
+              >
+                {cancelButton.text}
+              </Button>
+              <Button
+                colorScheme={confirmButton.colorScheme}
+                onClick={callbacks?.confirmDialog}
+                ml={3}
+              >
+                {confirmButton.text}
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+};
+
+export default {
+  ...createComponent({
+    version: "chakra_ui/v1",
+    metadata: {
+      name: "dialog",
+      description: "chakra_ui dialog",
+    },
+    spec: {
+      properties: [
+        {
+          name: "title",
+          ...TitlePropertySchema,
+        },
+        {
+          name: "confirmButton",
+          ...HandleButtonPropertySchema,
+        },
+        {
+          name: "cancelButton",
+          ...HandleButtonPropertySchema,
+        },
+      ],
+      acceptTraits: [],
+      state: {},
+      methods: [
+        {
+          name: "openDialog",
+          parameters: Type.Object({
+            title: Type.String(),
+          }),
+        },
+        {
+          name: "confirmDialog",
+        },
+        {
+          name: "cancelDialog",
+        },
+      ],
+    },
+  }),
+  impl: Dialog,
+};
