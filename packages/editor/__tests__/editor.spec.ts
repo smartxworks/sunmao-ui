be831e3a5091a6a6655ac7a02aea8b84e28a896f
Bowen Tan <bowen.tan@smartx.com>
2021-09-28 15:37:49 +0800

    fix test ci

---
+++ packages/editor/__tests__/editor.spec.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+describe('editor', () => {
+  test.todo('needs tests');
+});
--- packages/editor/__tests__/editor.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const editor = require('..');
-
-describe('editor', () => {
-  it('needs tests');
-});
