eceec4b4c673ca410b75970234734d8dad6c6b79
Bowen Tan <bowen.tan@smartx.com>
2021-10-12 11:42:50 +0800

    add grid bg and use react-resize-detector

--- packages/runtime/src/components/_internal/GridLayout.tsx
+++ packages/runtime/src/components/_internal/GridLayout.tsx
@@ -1,41 +1,41 @@
-import RGL, { WidthProvider } from 'react-grid-layout';
-import { Static, Type } from '@sinclair/typebox';
+import RGL from 'react-grid-layout';
+import { css } from '@emotion/react';
+import { useResizeDetector } from 'react-resize-detector';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import { GRID_HEIGHT } from '../../constants';
 
-const ReactGridLayout = WidthProvider(RGL);
+const GridLayout: React.FC<ReactGridLayout.ReactGridLayoutProps> = props => {
+  const { children } = props;
+  const spacing = 10;
+  const { width, ref } = useResizeDetector();
 
-export const LayoutPropertySchema = Type.Array(
-  Type.Object({
-    x: Type.Number(),
-    y: Type.Number(),
-    w: Type.Number(),
-    h: Type.Number(),
-    i: Type.String(),
-    isResizable: Type.Optional(Type.Boolean()),
-  })
-);
+  const bgCss = css`
+    height: 100%;
+    background: white;
+    background-image: linear-gradient(#eee 1px, transparent 0),
+      linear-gradient(90deg, #eee 1px, transparent 0);
+    background-size: ${(width || 0) / 12}px ${GRID_HEIGHT + spacing}px;
+    background-position: 0px ${spacing / 2}px;
+  `;
 
-const GridLayout: React.FC<{
-  layout: Static<typeof LayoutPropertySchema>;
-  onDragStop?: (layout: RGL.Layout[]) => void;
-  onDrop?: (layout: RGL.Layout[], item: RGL.Layout, event: DragEvent) => void;
-}> = ({ children, layout, onDragStop, onDrop }) => {
   return (
-    <ReactGridLayout
-      isDraggable={!!onDragStop}
-      isResizable={!!onDragStop}
-      compactType={null}
-      preventCollision={true}
-      rowHeight={GRID_HEIGHT}
-      layout={layout}
-      onDragStop={onDragStop}
-      onDrop={onDrop}
-      isDroppable={true}
-    >
-      {children}
-    </ReactGridLayout>
+    <div ref={ref} css={bgCss}>
+      <RGL
+        cols={12}
+        // isDraggable={!!onDragStop}
+        // isResizable={!!onDragStop}
+        compactType={null}
+        preventCollision={true}
+        rowHeight={GRID_HEIGHT}
+        width={width || 0}
+        margin={[spacing, spacing]}
+        isDroppable={true}
+        {...props}
+      >
+        {children}
+      </RGL>
+    </div>
   );
 };
 
