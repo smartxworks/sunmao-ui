7ab90ca6eceb2078aca82ff88ff7283a93e210a9
Bowen Tan <bowen.tan@smartx.com>
2021-10-15 15:13:18 +0800

    structure tree can toggle show children & hide slotName

--- packages/editor/src/components/StructureTree/ComponentItemView.tsx
+++ packages/editor/src/components/StructureTree/ComponentItemView.tsx
@@ -1,27 +1,53 @@
-import { DeleteIcon } from '@chakra-ui/icons';
-import { HStack, IconButton, Text } from '@chakra-ui/react';
+import { ChevronDownIcon, ChevronRightIcon, DeleteIcon } from '@chakra-ui/icons';
+import { Box, HStack, IconButton, Text } from '@chakra-ui/react';
 
 type Props = {
   title: string;
   isSelected: boolean;
   onClick: () => void;
   onClickRemove: () => void;
+  noChevron: boolean;
+  isExpanded?: boolean;
+  onToggleExpanded?: () => void;
 };
 
 export const ComponentItemView: React.FC<Props> = props => {
-  const { title, isSelected, onClick, onClickRemove } = props;
+  const {
+    title,
+    isSelected,
+    noChevron,
+    isExpanded,
+    onClick,
+    onToggleExpanded,
+    onClickRemove,
+  } = props;
+
+  const expandIcon = (
+    <IconButton
+      position="absolute"
+      left="-5"
+      aria-label="showChildren"
+      size="xs"
+      variant="unstyled"
+      onClick={onToggleExpanded}
+      icon={isExpanded ? <ChevronDownIcon /> : <ChevronRightIcon />}
+    />
+  );
   return (
-    <HStack width="full" justify="space-between">
-      <Text color={isSelected ? 'red.500' : 'black'} onClick={onClick}>
-        {title}
-      </Text>
-      <IconButton
-        variant="ghost"
-        size="sm"
-        aria-label="remove"
-        icon={<DeleteIcon />}
-        onClick={onClickRemove}
-      />
-    </HStack>
+    <Box width="full">
+      {noChevron ? null : expandIcon}
+      <HStack width="full" justify="space-between">
+        <Text color={isSelected ? 'red.500' : 'black'} onClick={onClick}>
+          {title}
+        </Text>
+        <IconButton
+          variant="ghost"
+          size="smx"
+          aria-label="remove"
+          icon={<DeleteIcon />}
+          onClick={onClickRemove}
+        />
+      </HStack>
+    </Box>
   );
 };
