bba8919c6b2144677a660ffad9e4b06fc2a71216
Bowen Tan <bowen.tan@smartx.com>
2021-10-20 14:46:24 +0800

    use formik implement event form

---
+++ packages/editor/src/components/ComponentForm/EventTraitForm/EventHandlerForm.tsx
@@ -0,0 +1,123 @@
+import { FormControl, FormLabel, Input, Select } from '@chakra-ui/react';
+import { Static } from '@sinclair/typebox';
+import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import { EventHandlerSchema } from '@meta-ui/runtime';
+import { registry } from '../../../metaUI';
+import { useAppModel } from '../../../operations/useAppModel';
+
+type Props = {
+  eventTypes: string[];
+  handler: Static<typeof EventHandlerSchema>;
+  onChange: (hanlder: Static<typeof EventHandlerSchema>) => void;
+};
+
+export const EventHandlerForm: React.FC<Props> = props => {
+  const { handler, eventTypes, onChange } = props;
+  const { app } = useAppModel();
+  const [methods, setMethods] = useState<string[]>([]);
+
+  function updateMethods(componentId: string) {
+    const type = app.spec.components.find(c => c.id === componentId)?.type;
+    if (type) {
+      const componentSpec = registry.getComponentByType(type).spec;
+      setMethods(componentSpec.methods.map(m => m.name));
+    }
+  }
+
+  useEffect(() => {
+    if (handler.componentId) {
+      updateMethods(handler.componentId);
+    }
+  }, [handler]);
+
+  const onTargetComponentChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    updateMethods(e.target.value);
+  };
+
+  const formik = useFormik({
+    initialValues: handler,
+    onSubmit: values => {
+      onChange(values);
+    },
+  });
+
+  const typeField = (
+    <FormControl>
+      <FormLabel>Event Type</FormLabel>
+      <Select
+        placeholder="Select Event Type"
+        onChange={formik.handleChange}
+        onBlur={() => formik.submitForm()}
+        value={formik.values.type}
+      >
+        {eventTypes.map(e => (
+          <option key={e} value={e}>
+            {e}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
+  );
+  const targetField = (
+    <FormControl>
+      <FormLabel>Target Component</FormLabel>
+      <Select
+        name="componentId"
+        placeholder="Select Target Component"
+        onChange={e => {
+          onTargetComponentChange(e);
+          formik.handleChange(e);
+        }}
+        onBlur={() => formik.submitForm()}
+        value={formik.values.componentId}
+      >
+        {app.spec.components.map(c => (
+          <option key={c.id} value={c.id}>
+            {c.id}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
+  );
+  const methodField = (
+    <FormControl>
+      <FormLabel>Method</FormLabel>
+      <Select
+        name="method.name"
+        placeholder="Select Method"
+        onChange={formik.handleChange}
+        onBlur={() => formik.submitForm()}
+        value={formik.values.method.name}
+      >
+        {methods.map(m => (
+          <option key={m} value={m}>
+            {m}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
+  );
+
+  const parametersField = (
+    <FormControl>
+      <FormLabel>Parameters</FormLabel>
+      <Input
+        name="method.parameters"
+        onChange={formik.handleChange}
+        onBlur={() => formik.submitForm()}
+        value={formik.values.method.parameters}
+      />
+    </FormControl>
+  );
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      {typeField}
+      {targetField}
+      {methodField}
+      {parametersField}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
---
+++ packages/editor/src/components/ComponentForm/EventTraitForm/EventTraitForm.tsx
@@ -0,0 +1,92 @@
+import { AddIcon } from '@chakra-ui/icons';
+import { Box, HStack, IconButton, VStack } from '@chakra-ui/react';
+import { Static } from '@sinclair/typebox';
+import { useMemo } from 'react';
+import { ApplicationComponent } from '@meta-ui/core';
+import { EventHandlerSchema } from '@meta-ui/runtime';
+import { eventBus } from '../../../eventBus';
+import { registry } from '../../../metaUI';
+import {
+  AddTraitOperation,
+  ModifyTraitPropertyOperation,
+} from '../../../operations/Operations';
+import { EventHandlerForm } from './EventHandlerForm';
+import produce from 'immer';
+
+type EventHandler = Static<typeof EventHandlerSchema>;
+
+type Props = {
+  component: ApplicationComponent;
+  handlers: EventHandler[];
+};
+
+export const EventTraitForm: React.FC<Props> = props => {
+  const { component, handlers } = props;
+
+  const eventTypes = useMemo(() => {
+    return registry.getComponentByType(component.type).spec.events;
+  }, [component]);
+
+  if (!eventTypes.length) return null;
+  const onClickAddHandler = () => {
+    const newHandler: EventHandler = {
+      type: eventTypes[0],
+      componentId: '',
+      method: {
+        name: '',
+        parameters: {},
+      },
+    };
+
+    if (handlers.length === 0) {
+      eventBus.send(
+        'operation',
+        new AddTraitOperation(component.id, 'core/v1/event', { handlers: [newHandler] })
+      );
+    } else {
+      eventBus.send(
+        'operation',
+        new ModifyTraitPropertyOperation(component.id, 'core/v1/event', 'handlers', [
+          ...handlers,
+          newHandler,
+        ])
+      );
+    }
+  };
+
+  const handlerForms = handlers.map((h, i) => {
+    const onChange = (handler: EventHandler) => {
+      const newHanlders = produce(handlers, draft => {
+        draft[i] = handler;
+      });
+      eventBus.send(
+        'operation',
+        new ModifyTraitPropertyOperation(
+          component.id,
+          'core/v1/event',
+          'handlers',
+          newHanlders
+        )
+      );
+    };
+    return (
+      <EventHandlerForm key={i} handler={h} eventTypes={eventTypes} onChange={onChange} />
+    );
+  });
+
+  return (
+    <VStack width="full">
+      <HStack width="full" justify="space-between">
+        <strong>Events</strong>
+        <IconButton
+          aria-label="add event"
+          size="sm"
+          variant="ghost"
+          icon={<AddIcon />}
+          onClick={onClickAddHandler}
+        />
+      </HStack>
+      <Box width="full">{handlerForms}</Box>
+    </VStack>
+  );
+};
