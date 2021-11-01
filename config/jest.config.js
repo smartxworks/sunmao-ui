8397358ee40f6abe59fbf48c3da0d2004a8c1308
Yanzhen Yu <yanzhen@smartx.com>
2021-11-01 23:43:27 +0800

    transform ES modules in node_modules

--- config/jest.config.js
+++ config/jest.config.js
@@ -5,4 +5,14 @@ module.exports = {
   collectCoverage: Boolean(process.env.COVERAGE),
   collectCoverageFrom: ['<rootDir>/src/**/*.ts?(x)'],
   coveragePathIgnorePatterns: ['generated'],
+  /**
+   * Since we use a lot of denepencies as ES moules, we need configure
+   * jest to transform the files in node_modules which export as
+   * ES modules.
+   * The list can be very long if we enumerate dependencies as a whitelist,
+   * so we use a blacklist to ignore certain dependencies that should
+   * not be transformed.
+   * This may cause performance issue.
+   */
+  transformIgnorePatterns: [`/node_modules/(jest|@testing-library)`],
 };
