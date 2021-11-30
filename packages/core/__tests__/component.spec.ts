428a77e5c41b99b5efff0706a084b3a82970fdf6
Bowen Tan <bowen.tan@smartx.com>
2021-11-30 10:35:09 +0800

    fix test

--- packages/core/__tests__/component.spec.ts
+++ packages/core/__tests__/component.spec.ts
@@ -1,85 +1,49 @@
+import { Type } from '@sinclair/typebox';
 import { createComponent } from '../src/component';
 
 describe('component', () => {
   it('can create runtime component', () => {
-    expect(
-      createComponent({
-        version: 'core/v1',
-        metadata: {
-          name: 'test_component',
-        },
-
-        spec: {
-          properties: [
-            {
-              name: 'x',
-              type: 'string',
-            },
-          ],
-
-          acceptTraits: [
-            {
-              name: 't1',
-            },
-          ],
-
-          state: {
-            type: 'string',
+    const c = createComponent({
+      version: 'core/v1',
+      metadata: {
+        isDraggable: true,
+        isResizable: true,
+        displayName: 'test_component',
+        name: 'test_component',
+        exampleProperties: {},
+        exampleSize: [1, 1],
+      },
+      spec: {
+        properties: Type.Object({
+          name: Type.String(),
+          type: Type.String(),
+        }),
+        state: Type.Object({
+          type: Type.String(),
+        }),
+        methods: [
+          {
+            name: 'reset',
           },
-
-          methods: [
-            {
-              name: 'reset',
+          {
+            name: 'add',
+            parameters: {
+              type: 'number',
             },
-
-            {
-              name: 'add',
-              parameters: {
-                type: 'number',
-              },
-            },
-          ],
-        },
-      })
-    ).toMatchInlineSnapshot(`
-      Object {
-        "kind": "Component",
-        "metadata": Object {
-          "name": "test_component",
-        },
-        "parsedVersion": Object {
-          "category": "core",
-          "value": "v1",
-        },
-        "spec": Object {
-          "acceptTraits": Array [
-            Object {
-              "name": "t1",
-            },
-          ],
-          "methods": Array [
-            Object {
-              "name": "reset",
-            },
-            Object {
-              "name": "add",
-              "parameters": Object {
-                "type": "number",
-              },
-            },
-          ],
-          "properties": Array [
-            Object {
-              "name": "x",
-              "type": "string",
-            },
-          ],
-          "state": Object {
-            "type": "string",
           },
-        },
-        "version": "core/v1",
-      }
-    `);
+        ],
+        styleSlots: ['content'],
+        slots: [],
+        events: [],
+      },
+    });
+    expect(c).toMatchObject({
+      ...c,
+      kind: 'Component',
+      parsedVersion: {
+        category: 'core',
+        value: 'v1',
+      },
+    });
   });
 });
