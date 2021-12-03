5cc864db4d3c095b46f34c2f0be4d54ca76241d7
Bowen Tan <bowen.tan@smartx.com>
2021-12-03 15:44:18 +0800

    add cut paste

--- packages/editor/src/operations/branch/pasteComponentBranchOperation.ts
+++ packages/editor/src/operations/branch/pasteComponentBranchOperation.ts
@@ -1,79 +1,38 @@
 import { ApplicationComponent } from '@sunmao-ui/core';
-import { CreateComponentBranchOperation } from '../branch';
+import produce from 'immer';
+import { PasteComponentLeafOperation } from '../leaf/component/pasteComponentLeafOperation';
 import { BaseBranchOperation } from '../type';
 
 export type PasteComponentBranchOperationContext = {
   parentId: string;
   slot: string;
-  componentId: string;
+  components: ApplicationComponent[];
 };
 
 export class PasteComponentBranchOperation extends BaseBranchOperation<PasteComponentBranchOperationContext> {
   do(prev: ApplicationComponent[]): ApplicationComponent[] {
-    console.log('paste', this.context)
-    // find component to remove
-    const targetComponent = prev.find(c => c.id === this.context.componentId);
-    if (!targetComponent) {
-      console.warn('paste component not found');
-      return prev;
-    }
-    // TODO: Change slot to target Parent
-    // this.operationStack.insert(
-    //   new CreateComponentBranchOperation({ componentId: component.id })
-    // );
-
-    // TODO: O(n2) in worst case. Optimize in the future.
-    const children = prev.filter(c => {
-      const slotTrait = c.traits.find(t => t.type === 'core/v1/slot');
-      return (
-        slotTrait &&
-        (slotTrait.properties.container as { id: string }).id === this.context.componentId
+    const newComponents = produce(this.context.components, draft => {
+      const newSlotTrait = {
+        type: 'core/v1/slot',
+        properties: { container: { id: this.context.parentId, slot: this.context.slot } },
+      };
+      const oldSlotIndex = draft[0].traits.findIndex(
+        trait => trait.type === 'core/v1/slot'
       );
+      if (oldSlotIndex !== -1) {
+        draft[0].traits.push(newSlotTrait);
+      } else {
+        draft[0].traits[oldSlotIndex] = newSlotTrait;
+      }
     });
-
-    children.forEach(component => {
+    console.log('paste', newComponents)
+    newComponents.forEach(c => {
       this.operationStack.insert(
-        new CreateComponentBranchOperation({
-          componentId: `${component.id}_copy`,
-          componentType: component.type,
-          parentId: `${this.context.componentId}_copy`,
-          slot: this.context.slot,
+        new PasteComponentLeafOperation({
+          component: c,
         })
       );
     });
-
-    // TODO: dont care about layout component
-    // if (parentId) {
-    //   // modify layout property of parent grid layout component
-    //   this.operationStack.insert(
-    //     new ModifyComponentPropertiesLeafOperation({
-    //       componentId: parentId,
-    //       properties: {
-    //         layout: (prev: Array<ReactGridLayout.Layout>) => {
-    //           return produce(prev, draft => {
-    //             const removeIndex = draft.findIndex(
-    //               item => item.i === this.context.componentId
-    //             );
-    //             if (removeIndex === -1) {
-    //               console.warn("parent element doesn' contain specific child");
-    //             }
-    //             draft.splice(removeIndex, 1);
-    //           });
-    //         },
-    //       },
-    //     })
-    //   );
-    // }
-
-    // free component from schema
-    this.operationStack.insert(
-      new CreateComponentBranchOperation({
-        componentId: `${this.context.componentId}_copy`,
-        componentType: targetComponent.type,
-        parentId: this.context.parentId,
-        slot: this.context.slot,
-      })
-    );
 
     // do the operation in order
     return this.operationStack.reduce((prev, node) => {
