7ed3712125f6420e4b3bd0f157f435bd466f507b
Bowen Tan <bowen.tan@smartx.com>
2021-12-09 10:10:37 +0800

    Revert "add vite-tsconfig-path plugin"
    This reverts commit 504f8118ea22587205a6b9abdf0cca9c45ca755c.

--- packages/runtime/vite.config.ts
+++ packages/runtime/vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { virtualExamplePlugin } from '@sunmao-ui/vite-plugins';
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     react({
       babel: {
         plugins: [
