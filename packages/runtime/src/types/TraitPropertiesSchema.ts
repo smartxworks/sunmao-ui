71e0d79095206e07d2771daaac0a860f0deb22bb
Bowen Tan <bowen.tan@smartx.com>
2021-12-13 15:04:51 +0800

    make some components property option

--- packages/runtime/src/types/TraitPropertiesSchema.ts
+++ packages/runtime/src/types/TraitPropertiesSchema.ts
@@ -1,29 +1,32 @@
 import { Type } from '@sinclair/typebox';
 
-export const EventHandlerSchema = Type.Object(
-  {
-    type: Type.String(),
-    componentId: Type.String(),
-    method: Type.Object({
-      name: Type.String(),
-      parameters: Type.Optional(Type.Record(Type.String(), Type.Any())),
-    }),
-    wait: Type.Optional(
-      Type.Object({
-        type: Type.KeyOf(
-          Type.Object({
-            debounce: Type.String(),
-            throttle: Type.String(),
-            delay: Type.String(),
-          })
-        ),
-        time: Type.Number(),
-      })
-    ),
-    disabled: Type.Optional(Type.Boolean()),
-  },
-  { description: 'eventHanlder' }
-);
+const BaseEventSchema = {
+  componentId: Type.String(),
+  method: Type.Object({
+    name: Type.String(),
+    parameters: Type.Optional(Type.Record(Type.String(), Type.Any())),
+  }),
+  wait: Type.Optional(
+    Type.Object({
+      type: Type.KeyOf(
+        Type.Object({
+          debounce: Type.String(),
+          throttle: Type.String(),
+          delay: Type.String(),
+        })
+      ),
+      time: Type.Number(),
+    })
+  ),
+  disabled: Type.Optional(Type.Boolean()),
+};
+
+export const EventHandlerSchema = Type.Object({
+  type: Type.String(),
+  ...BaseEventSchema,
+});
+
+export const EventCallBackHandlerSchema = Type.Object(BaseEventSchema);
 
 export const FetchTraitPropertiesSchema = Type.Object({
   url: Type.String(), // {format:uri}?;
@@ -31,5 +34,6 @@ export const FetchTraitPropertiesSchema = Type.Object({
   lazy: Type.Optional(Type.Boolean()),
   headers: Type.Record(Type.String(), Type.String()),
   body: Type.Record(Type.String(), Type.String()),
-  onComplete: Type.Optional(Type.Array(EventHandlerSchema)),
+  onComplete: Type.Optional(Type.Array(EventCallBackHandlerSchema)),
+  onError: Type.Optional(Type.Array(EventCallBackHandlerSchema)),
 });
