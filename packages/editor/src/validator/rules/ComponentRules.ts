50614ab04ab820adaa9f7ff1b352f28f3ebd2d09
Bowen Tan <bowen.tan@smartx.com>
2021-12-15 15:43:18 +0800

    add module validator

--- packages/editor/src/validator/rules/ComponentRules.ts
+++ packages/editor/src/validator/rules/ComponentRules.ts
@@ -42,4 +42,33 @@ class ComponentPropertyValidatorRule implements ComponentValidatorRule {
     return results;
   }
 }
-export const ComponentRules = [new ComponentPropertyValidatorRule()];
+
+class ModuleValidatorRule implements ComponentValidatorRule {
+  kind: 'component' = 'component';
+
+  validate({ component, registry }: ComponentValidateContext): ValidateErrorResult[] {
+    if (component.type !== 'core/v1/moduleContainer') {
+      return [];
+    }
+
+    const results: ValidateErrorResult[] = [];
+    let moduleSpec
+    try {
+      moduleSpec = registry.getModuleByType(component.properties.type as string);
+    } catch (err) {
+      moduleSpec = undefined
+    }
+    if (!moduleSpec) {
+      results.push({
+        message: `Module is not registered: ${component.properties.type}.`,
+        componentId: component.id,
+        property: '/type',
+      });
+    }
+    return results;
+  }
+}
+export const ComponentRules = [
+  new ComponentPropertyValidatorRule(),
+  new ModuleValidatorRule(),
+];
