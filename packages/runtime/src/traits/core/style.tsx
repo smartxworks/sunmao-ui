aac9003b9da3f5c3286bf7e2be3189b365b96a89
Bowen Tan <bowen.tan@smartx.com>
2021-12-16 17:14:06 +0800

    change style trait params to array

--- packages/runtime/src/traits/core/style.tsx
+++ packages/runtime/src/traits/core/style.tsx
@@ -2,22 +2,25 @@ import { createTrait } from '@sunmao-ui/core';
 import { Static, Type } from '@sinclair/typebox';
 import { TraitImplementation } from '../../types/RuntimeSchema';
 
-const StyleTrait: TraitImplementation<Static<typeof PropsSchema>> = ({
-  styleSlot,
-  style,
-}) => {
+const StyleTrait: TraitImplementation<Static<typeof PropsSchema>> = ({ styles }) => {
+  const customStyle: Record<string, string> = {};
+  styles.forEach(style => {
+    customStyle[style.styleSlot] = style.style;
+  });
   return {
     props: {
-      customStyle: {
-        [styleSlot]: style,
-      },
+      customStyle,
     },
   };
 };
 
 const PropsSchema = Type.Object({
-  styleSlot: Type.String(),
-  style: Type.String(),
+  styles: Type.Array(
+    Type.Object({
+      styleSlot: Type.String(),
+      style: Type.String(),
+    })
+  ),
 });
 
 export default {
