e46dc66bac8ac5c555f0bf38f2eab1904ea6964b
Bowen Tan <bowen.tan@smartx.com>
2021-12-21 14:07:03 +0800

    add some operations

--- packages/editor/src/operations/AppModel/AppModel.ts
+++ packages/editor/src/operations/AppModel/AppModel.ts
@@ -1,4 +1,7 @@
-import { ApplicationComponent } from '@sunmao-ui/core';
+import { ApplicationComponent, ComponentTrait } from '@sunmao-ui/core';
+import { parseType } from '@sunmao-ui/runtime';
+import produce from 'immer';
+import { registry } from '../../setup';
 import { ComponentModel } from './ComponentModel';
 import {
   ComponentId,
@@ -9,22 +12,31 @@ import {
   ModuleId,
   ModuleType,
   SlotName,
+  TraitType,
 } from './IAppModel';
+import { genComponent } from './utils';
+
+const SlotTraitType: TraitType = 'core/v1/slot' as TraitType;
 export class ApplicationModel implements IApplicationModel {
-  components: IComponentModel[] = [];
+  model: IComponentModel[] = [];
   modules: IModuleModel[] = [];
   allComponents: IComponentModel[] = [];
-  private origin: ApplicationComponent[] = [];
+  private schema: ApplicationComponent[] = [];
   private componentMap: Record<ComponentId, IComponentModel> = {};
 
   constructor(components: ApplicationComponent[]) {
-    this.origin = components;
+    this.schema = components;
     this.resolveTree(components);
+  }
+
+  updateSingleComponent(component: IComponentModel) {
+    this.componentMap[component.id] = component;
+    this.allComponents.push(component);
   }
 
   resolveTree(components: ApplicationComponent[]) {
     this.allComponents = components.map(c => {
-      const comp = new ComponentModel(c);
+      const comp = new ComponentModel(this, c);
       this.componentMap[c.id as ComponentId] = comp;
       return comp;
     });
@@ -41,29 +53,118 @@ export class ApplicationModel implements IApplicationModel {
         }
         child.parent = parent;
       } else {
-        this.components.push(child);
+        this.model.push(child);
       }
     });
   }
 
   get json(): ApplicationComponent[] {
-    return this.origin;
+    // if (this.allComponents.length !== this.schema.length) {
+    //   return this.allComponents.map(c => c.json);
+    // }
+
+    // if (this.allComponents.some(c => c.isDirty)) {
+    //   return this.allComponents.map(c => c.json);
+    // }
+
+    // for (let i = 0; i < this.schema.length - 1; i++) {
+    //   const comp = this.schema[i];
+    //   const component = this.componentMap[comp.id as ComponentId];
+    //   if (component.isDirty) {
+    //     this.schema.splice(i + 1, 1, component.json);
+    //   }
+    // }
+
+    this.schema = this.allComponents.map(c => {
+      return c.json;
+    });
+
+    return this.schema;
   }
-  // createComponent (
+
+  createComponent(type: ComponentType): IComponentModel {
+    const component = genComponent(type, this.genId(type), {}, []);
+    return new ComponentModel(this, component);
+  }
+
+  genId(type: ComponentType): ComponentId {
+    const { name } = parseType(type);
+    const componentsCount = this.allComponents.filter(
+      component => component.type === type
+    ).length;
+    return `${name}${componentsCount + 1}` as ComponentId;
+  }
+
+  getComponentById(componentId: ComponentId): IComponentModel | undefined {
+    return this.componentMap[componentId];
+  }
+
+  findComponentIndex(componentId: ComponentId): number {
+    return this.schema.findIndex(c => c.id === componentId);
+  }
+
+  private updateSchema(schema: ApplicationComponent[]) {
+    this.schema = schema;
+    this.model = [];
+    this.allComponents = [];
+    this.componentMap = {};
+    this.resolveTree(schema);
+  }
+
+  // createComponent(
   //   componentType: ComponentType,
   //   componentId: ComponentId,
   //   properties: Record<string, string>
   // ) {
-  //   return
+  //   const component = genComponent(componentType, componentId, properties);
+  //   const newSchema = produce(this.schema, draft => {
+  //     draft.push(component);
+  //   });
+  //   this.updateSchema(newSchema);
+  //   return newSchema;
   // }
+
+  removeComponent(componentId: ComponentId) {
+    const newSchema = this.schema.filter(c => c.id !== componentId);
+    this.updateSchema(newSchema);
+    return newSchema;
+  }
   // createModule: (moduleId: ModuleId, moduleType: ModuleType) => IModuleModel;
-  // removeComponent: (componentId: ComponentId) => void;
   // removeModule: (moduleId: ModuleId) => void;
-  // findComponent: (componentId: ComponentId) => IComponentModel | undefined;
-  // moveComponent: (
-  //   fromId: ComponentId,
-  //   toId: ComponentId,
-  //   slot: SlotName,
-  //   afterId: ComponentId
-  // ) => void;
+  moveComponent(
+    fromId: ComponentId,
+    toId: ComponentId,
+    slot: SlotName,
+    afterId?: ComponentId
+  ) {
+    const fromIndex = this.findComponentIndex(fromId);
+    const afterIndex = afterId
+      ? this.findComponentIndex(afterId)
+      : this.findComponentIndex(toId);
+    this.changeTraitProperties(fromId, SlotTraitType, { container: { id: toId, slot } });
+
+    const newSchema = produce(this.schema, draft => {
+      const target = draft.splice(fromIndex, 1)[0];
+      draft.splice(fromIndex >= afterIndex ? afterIndex + 1 : afterIndex, 0, target);
+    });
+    this.updateSchema(newSchema);
+    return this.schema;
+  }
+
+  changeTraitProperties(
+    componentId: ComponentId,
+    traitType: TraitType,
+    properties: Record<string, unknown>
+  ) {
+    const componentIndex = this.findComponentIndex(componentId);
+    const component = this.schema[componentIndex];
+    const traitIndex = component?.traits.findIndex(t => t.type === traitType);
+    if (traitIndex > -1) {
+      const newSchema = produce(this.schema, draft => {
+        draft[componentIndex].traits[traitIndex].properties = properties;
+      });
+      this.updateSchema(newSchema);
+    }
+    return this.schema;
+  }
 }
--- packages/editor/src/operations/AppModel/ComponentModel.ts
+++ packages/editor/src/operations/AppModel/ComponentModel.ts
@@ -1,5 +1,6 @@
 import { ApplicationComponent, RuntimeComponentSpec } from '@sunmao-ui/core';
 import { registry } from '../../setup';
+import { genComponent, genTrait, getPropertyObject } from './utils';
 import {
   ComponentId,
   ComponentType,
@@ -15,11 +16,13 @@ import {
   ITraitModel,
   IFieldModel,
   EventName,
+  TraitType,
 } from './IAppModel';
 import { TraitModel } from './TraitModel';
+import { FieldModel } from './FieldModel';
 
+const SlotTraitType: TraitType = 'core/v1/slot' as TraitType;
 export class ComponentModel implements IComponentModel {
-  private origin: ApplicationComponent;
   private spec: RuntimeComponentSpec;
 
   id: ComponentId;
@@ -30,22 +33,27 @@ export class ComponentModel implements IComponentModel {
   parentId: ComponentId | null = null;
   parentSlot: SlotName | null = null;
   traits: ITraitModel[] = [];
+  isDirty = false;
 
-  constructor(component: ApplicationComponent) {
-    this.origin = component;
+  constructor(public appModel: IApplicationModel, private schema: ApplicationComponent) {
+    this.schema = schema;
 
-    this.id = component.id as ComponentId;
-    this.type = component.type as ComponentType;
+    this.id = schema.id as ComponentId;
+    this.type = schema.type as ComponentType;
     this.spec = registry.getComponentByType(this.type);
 
-    this.traits = component.traits.map(t => new TraitModel(t, this));
+    this.traits = schema.traits.map(t => new TraitModel(t, this));
     // find slot trait
     this.traits.forEach(t => {
       if (t.type === 'core/v1/slot') {
         this.parentId = t.properties.container.id;
         this.parentSlot = t.properties.container.slot;
       }
-    })
+    });
+
+    for (const key in schema.properties) {
+      this.properties[key] = new FieldModel(schema.properties[key]);
+    }
   }
 
   get slots() {
@@ -59,7 +67,7 @@ export class ComponentModel implements IComponentModel {
       (acc, t) => acc.concat(t.stateKeys),
       [] as StateKey[]
     );
-    return [...componentStateKeys, ...traitStateKeys]
+    return [...componentStateKeys, ...traitStateKeys];
   }
 
   get events() {
@@ -73,7 +81,7 @@ export class ComponentModel implements IComponentModel {
       (acc, t) => acc.concat(t.methods),
       [] as MethodName[]
     );
-    return [...componentMethods, ...traitMethods]
+    return [...componentMethods, ...traitMethods];
   }
 
   get styleSlots() {
@@ -81,6 +89,67 @@ export class ComponentModel implements IComponentModel {
   }
 
   get json(): ApplicationComponent {
-    return this.origin;
+    if (!this.isDirty) {
+      return this.schema;
+    }
+    this.isDirty = false;
+    const newProperties = getPropertyObject(this.properties);
+    const newTraits = this.traits.map(t => t.json);
+    const newSchema = genComponent(this.type, this.id, newProperties, newTraits);
+    this.schema = newSchema;
+    return this.schema;
+
+    // if (this.isDirty || this.traits.length !== this.origin.traits.length) {
+    //   return {
+    //     ...this.origin,
+    //     traits: this.traits.map(t => t.json),
+    //   };
+    // } else {
+    //   const isChanged = this.traits.some(t => t.isDirty);
+    //   if (isChanged) {
+    //     return {
+    //       ...this.origin,
+    //       traits: this.traits.map(t => t.json),
+    //     };
+    //   }
+    // }
+
+    // return this.origin;
   }
+
+  changeComponentProperty(propertyName: string, value: any) {
+    this.properties[propertyName].update(value);
+    this.isDirty = true;
+    // const newSchema = produce(this.schema, draft => {
+    //   draft[componentIndex].properties[propertyName] = value;
+    // });
+    // this.updateSchema(newSchema)
+  }
+
+  addTrait(traitType: TraitType, properties: Record<string, unknown>): ITraitModel {
+    const traitSchema = genTrait(traitType, properties);
+    const trait = new TraitModel(traitSchema, this);
+    this.traits.push(trait);
+    this.isDirty = true;
+    return trait;
+  }
+
+  appendTo = (slot: SlotName, parent: IComponentModel) => {
+    if (!parent.children[slot]) {
+      parent.children[slot] = [];
+    }
+
+    // update parent
+    parent.children[slot].push(this);
+    this.parent = parent;
+    this.parentSlot = slot;
+    this.parentId = parent.id;
+
+    // update app model
+    this.appModel.updateSingleComponent(this);
+
+    // update trait
+    this.addTrait(SlotTraitType, { container: { id: parent.id, slot } });
+    this.isDirty = true;
+  };
 }
