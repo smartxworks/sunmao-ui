78473df7fe1f222987b7de7fbd12892a832b5d2a
tanbowensg <bowen.tan@smartx.com>
2022-01-20 16:51:48 +0800

    Merge pull request #8 from webzard-io/dropdown
    feat: implement dropdown

--- packages/arco-lib/src/components/Dropdown.tsx
+++ packages/arco-lib/src/components/Dropdown.tsx
@@ -1,34 +1,84 @@
-import { Dropdown as BaseDropdown } from "@arco-design/web-react";
+import { 
+  Dropdown as BaseDropdown, 
+  Menu as BaseMenu 
+} from "@arco-design/web-react";
 import {
   ComponentImpl,
   implementRuntimeComponent,
 } from "@sunmao-ui/runtime";
 import { Type, Static } from "@sinclair/typebox";
 import { FALLBACK_METADATA, getComponentProps } from "../sunmao-helper";
-import { DropdownPropsSchema as BaseDropdownPropsSchema } from "../generated/types/Dropdown";
+import { 
+  DropdownPropsSchema as BaseDropdownPropsSchema 
+} from "../generated/types/Dropdown";
 
 const DropdownPropsSchema = Type.Object(BaseDropdownPropsSchema);
-const DropdownStateSchema = Type.Object({});
+const DropdownStateSchema = Type.Object({
+  selectedItemKey: Type.String(),
+  keyPath: Type.Optional(Type.Array(Type.String())),
+  visible: Type.Boolean()
+});
 
 const DropdownImpl: ComponentImpl<
   Static<typeof DropdownPropsSchema>
 > = (props) => {
-  const { slotsElements } = props;
+  const { 
+    slotsElements,
+    callbackMap,
+    mergeState
+  } = props;
   const cProps = getComponentProps(props);
+  const { list, dropdownType, ...restProps } = cProps;
+  const typeMap = {
+    default: BaseDropdown,
+    button: BaseDropdown.Button,
+  };
+
+  const onClickMenuItem = (key: string, event: any, keyPath: string[])=> {
+    mergeState({
+      selectedItemKey: key,
+      keyPath: keyPath || []
+    });
+    callbackMap?.onClickMenuItem?.();
+  }
+  const onVisibleChange = (visible: boolean)=> {
+    mergeState({
+      visible
+    });
+    callbackMap?.onVisibleChange?.();
+  }
+
+  const Dropdown = typeMap[dropdownType];
+  const droplist = (
+    <BaseMenu onClickMenuItem={onClickMenuItem}>
+      {
+        (list || []).map((item)=> (
+          <BaseMenu.Item key={item.key}>{item.label}</BaseMenu.Item>
+        ))
+      }
+    </BaseMenu>
+  );
 
   return (
-    <BaseDropdown {...cProps} droplist={slotsElements.list}>
+    <Dropdown 
+      {...restProps}
+      droplist={droplist}
+      onVisibleChange={onVisibleChange}
+      onClick={callbackMap?.onButtonClick}
+    >
       {slotsElements.trigger}
-    </BaseDropdown>
+    </Dropdown>
   );
 };
 
 const exampleProperties: Static<typeof DropdownPropsSchema> = {
+  dropdownType: 'default',
+  trigger: 'hover',
   position: "bl",
   disabled: false,
   unmountOnExit: false,
   defaultPopupVisible: false,
-  popupVisible: false,
+  list: []
 };
 
 const options = {
@@ -43,9 +93,13 @@ const options = {
     properties: DropdownPropsSchema,
     state: DropdownStateSchema,
     methods: {},
-    slots: ["trigger", "list"],
+    slots: ['trigger'],
     styleSlots: [],
-    events: [],
+    events: [
+      'onClickMenuItem', 
+      'onVisibleChange',
+      'onButtonClick'
+    ],
   },
 };
 
--- packages/arco-lib/src/generated/types/Dropdown.ts
+++ packages/arco-lib/src/generated/types/Dropdown.ts
@@ -3,9 +3,14 @@ import { Type } from "@sinclair/typebox";
 import { IntoStringUnion, StringUnion } from '../../sunmao-helper';
 
 export const DropdownPropsSchema = {
+  'dropdownType': StringUnion(['default', 'button']),
   'position': Type.Optional(StringUnion(['top', 'tl', 'tr', 'bottom', 'bl', 'br'])),
+  'trigger': StringUnion(['hover', 'click']),
   'disabled': Type.Optional(Type.Boolean()),
   'unmountOnExit': Type.Optional(Type.Boolean()),
   'defaultPopupVisible': Type.Optional(Type.Boolean()),
-  'popupVisible': Type.Optional(Type.Boolean())
+  'list': Type.Optional(Type.Array(Type.Object({
+    key: Type.String(),
+    label: Type.String(),
+  })))
 };
