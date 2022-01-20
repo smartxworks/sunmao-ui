d33a976c2becc3026d38d1ee1c963b685cfef1fe
Bowen Tan <bowen.tan@smartx.com>
2022-01-20 10:33:48 +0800

    fix string union type

--- packages/arco-lib/src/generated/types/Link.ts
+++ packages/arco-lib/src/generated/types/Link.ts
@@ -5,7 +5,7 @@ export const LinkPropsSchema = {
     className: Type.Optional(Type.String()),
     disabled: Type.Optional(Type.Boolean()),
     hoverable: Type.Optional(Type.Boolean()),
-    status: Type.Optional(StringUnion(['default', 'success', 'warning', 'error'])),
+    status: Type.Optional(StringUnion(['success', 'warning', 'error'])),
     href: Type.Optional(Type.String()),
     content: Type.Optional(Type.String())
 }
