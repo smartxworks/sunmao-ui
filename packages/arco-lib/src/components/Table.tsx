d6f9bc26f13c85c57655e5a1fe4eb4ecf216ea59
MrWindlike <MrWindlike@gmail.com>
2022-02-09 10:23:39 +0800

    Merge pull request #21 from webzard-io/bugfix
    fix(table): add pagesize

--- packages/arco-lib/src/components/Table.tsx
+++ packages/arco-lib/src/components/Table.tsx
@@ -195,6 +195,7 @@ const TableImpl: ComponentImpl<Static<typeof TablePropsSchema>> = (props) => {
       pagination={{
         total: sortedData!.length,
         current: currentPage,
+        pageSize
       }}
       data={currentPageData}
       onChange={handleChange}
