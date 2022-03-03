3d1b1549c5e63438095235f1081e05f88535e593
Bowen Tan <bowen.tan@smartx.com>
2022-03-03 11:18:51 +0800

    remove editor from arco-lib's dependencies

--- packages/arco-lib/vite.config.ts
+++ packages/arco-lib/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +8,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(__dirname, "index.html"),
-        examples: resolve(__dirname, "examples.html"),
+        index: resolve(__dirname, 'index.html'),
       },
     },
   },
