b19010e9ae228ba78ccc4f3c395becc331756e1f
Bowen Tan <bowen.tan@smartx.com>
2022-03-07 14:26:47 +0800

    add TraitImplFactory

--- packages/runtime/src/traits/core/Validation.tsx
+++ packages/runtime/src/traits/core/Validation.tsx
@@ -1,86 +1,87 @@
 import { createTrait } from '@sunmao-ui/core';
 import { Static, Type } from '@sinclair/typebox';
 import { isEqual } from 'lodash-es';
-import { TraitImpl } from '../../types';
+import { TraitImplFactory } from '../../types';
 import { ValidResultSchema } from '../../types/validResultSchema';
 
 type ValidationResult = Static<typeof ValidResultSchema>;
 type ValidationRule = (text: string) => ValidationResult;
 
-const rules = new Map<string, ValidationRule>();
+const ValidationTraitFactory: TraitImplFactory<Static<typeof PropsSchema>> = () => {
+  const rules = new Map<string, ValidationRule>();
 
-export function addValidationRule(name: string, rule: ValidationRule) {
-  rules.set(name, rule);
-}
+  function addValidationRule(name: string, rule: ValidationRule) {
+    rules.set(name, rule);
+  }
 
-addValidationRule('email', text => {
-  if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(text)) {
-    return {
+  addValidationRule('email', text => {
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(text)) {
+      return {
+        isInvalid: false,
+        errorMsg: '',
+      };
+    } else {
+      return {
+        isInvalid: true,
+        errorMsg: '请输入正确的 email',
+      };
+    }
+  });
+
+  addValidationRule('phoneNumber', text => {
+    if (/^1[3456789]\d{9}$/.test(text)) {
+      return {
+        isInvalid: false,
+        errorMsg: '',
+      };
+    } else {
+      return {
+        isInvalid: true,
+        errorMsg: '请输入正确的手机号码',
+      };
+    }
+  });
+  const ValidationResultCache: Record<string, ValidationResult> = {};
+
+  return props => {
+    const { value, minLength, maxLength, mergeState, componentId, rule } = props;
+
+    const result: ValidationResult = {
       isInvalid: false,
       errorMsg: '',
     };
-  } else {
-    return {
-      isInvalid: true,
-      errorMsg: '请输入正确的 email',
-    };
-  }
-});
 
-addValidationRule('phoneNumber', text => {
-  if (/^1[3456789]\d{9}$/.test(text)) {
-    return {
-      isInvalid: false,
-      errorMsg: '',
-    };
-  } else {
-    return {
-      isInvalid: true,
-      errorMsg: '请输入正确的手机号码',
-    };
-  }
-});
-
-const ValidationResultCache: Record<string, ValidationResult> = {};
-
-const ValidationTraitImpl: TraitImpl<Static<typeof PropsSchema>> = props => {
-  const { value, minLength, maxLength, mergeState, componentId, rule } = props;
-
-  const result: ValidationResult = {
-    isInvalid: false,
-    errorMsg: '',
-  };
-
-  if (maxLength !== undefined && value.length > maxLength) {
-    result.isInvalid = true;
-    result.errorMsg = `最长不能超过${maxLength}个字符`;
-  } else if (minLength !== undefined && value.length < minLength) {
-    result.isInvalid = true;
-    result.errorMsg = `不能少于${minLength}个字符`;
-  } else {
-    const rulesArr = rule ? rule.split(',') : [];
-    for (const ruleName of rulesArr) {
-      const validateFunc = rules.get(ruleName);
-      if (validateFunc) {
-        const { isInvalid, errorMsg } = validateFunc(value);
-        if (isInvalid) {
-          result.isInvalid = true;
-          result.errorMsg = errorMsg;
-          break;
+    if (maxLength !== undefined && value.length > maxLength) {
+      result.isInvalid = true;
+      result.errorMsg = `最长不能超过${maxLength}个字符`;
+    } else if (minLength !== undefined && value.length < minLength) {
+      result.isInvalid = true;
+      result.errorMsg = `不能少于${minLength}个字符`;
+    } else {
+      const rulesArr = rule ? rule.split(',') : [];
+      for (const ruleName of rulesArr) {
+        const validateFunc = rules.get(ruleName);
+        if (validateFunc) {
+          const { isInvalid, errorMsg } = validateFunc(value);
+          if (isInvalid) {
+            result.isInvalid = true;
+            result.errorMsg = errorMsg;
+            break;
+          }
         }
       }
     }
-  }
 
-  if (!isEqual(result, ValidationResultCache[componentId])) {
-    ValidationResultCache[componentId] = result;
-    mergeState({
-      validResult: result,
-    });
-  }
+    if (!isEqual(result, ValidationResultCache[componentId])) {
+      ValidationResultCache[componentId] = result;
+      mergeState({
+        validResult: result,
+      });
+    }
 
-  return {
-    props: null,
+    return {
+      props: null,
+    };
   };
 };
 
@@ -106,5 +107,5 @@ export default {
       methods: [],
     },
   }),
-  impl: ValidationTraitImpl,
+  factory: ValidationTraitFactory,
 };
