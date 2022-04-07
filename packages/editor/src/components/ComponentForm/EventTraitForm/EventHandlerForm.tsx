af2780a7e7163de14772df094edd5ec05f17b801
MrWindlike <MrWindlike@gmail.com>
2022-04-07 16:40:41 +0800

    feat: add handlers sort

--- packages/editor/src/components/ComponentForm/EventTraitForm/EventHandlerForm.tsx
+++ packages/editor/src/components/ComponentForm/EventTraitForm/EventHandlerForm.tsx
@@ -3,23 +3,38 @@ import { EventWidget } from '@sunmao-ui/editor-sdk';
 import { Box, IconButton, VStack } from '@chakra-ui/react';
 import { Static } from '@sinclair/typebox';
 import { JSONSchema7 } from 'json-schema';
-import { CloseIcon } from '@chakra-ui/icons';
+import { CloseIcon, ArrowUpIcon, ArrowDownIcon } from '@chakra-ui/icons';
 import { EventHandlerSchema } from '@sunmao-ui/runtime';
 import { ComponentSchema } from '@sunmao-ui/core';
 import { formWrapperCSS } from '../style';
 import { EditorServices } from '../../../types';
 
 type Props = {
+  index: number;
+  size: number;
   component: ComponentSchema;
   handler: Static<typeof EventHandlerSchema>;
   onChange: (handler: Static<typeof EventHandlerSchema>) => void;
   onRemove: () => void;
+  onUp: () => void;
+  onDown: () => void;
   services: EditorServices;
   schema?: JSONSchema7;
 };
 
 export const EventHandlerForm: React.FC<Props> = props => {
-  const { schema = EventHandlerSchema, handler, component, onChange, onRemove, services } = props;
+  const {
+    index,
+    size,
+    schema = EventHandlerSchema,
+    handler,
+    component,
+    services,
+    onChange,
+    onRemove,
+    onUp,
+    onDown,
+  } = props;
 
   return (
     <Box position="relative" width="100%">
@@ -34,17 +49,32 @@ export const EventHandlerForm: React.FC<Props> = props => {
           onChange={onChange}
         />
       </VStack>
-      <IconButton
-        aria-label="remove event handler"
-        colorScheme="red"
-        icon={<CloseIcon />}
-        onClick={onRemove}
-        position="absolute"
-        right="4"
-        size="xs"
-        top="4"
-        variant="ghost"
-      />
+      <Box position="absolute" right="4" top="4">
+        <IconButton
+          aria-label="up event handler"
+          icon={<ArrowUpIcon />}
+          size="xs"
+          variant="ghost"
+          disabled={index === 0}
+          onClick={onUp}
+        />
+        <IconButton
+          aria-label="down event handler"
+          icon={<ArrowDownIcon />}
+          size="xs"
+          variant="ghost"
+          disabled={index === size - 1}
+          onClick={onDown}
+        />
+        <IconButton
+          aria-label="remove event handler"
+          colorScheme="red"
+          icon={<CloseIcon />}
+          onClick={onRemove}
+          size="xs"
+          variant="ghost"
+        />
+      </Box>
     </Box>
   );
 };
