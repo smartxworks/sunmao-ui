523cc09f07bea4a9e0f1fd9ec455ee2aa9d57ff7
xzdry <dry@justdry.net>
2022-04-08 10:02:46 +0800

    refactor avatar component

--- packages/arco-lib/src/components/Avatar.tsx
+++ packages/arco-lib/src/components/Avatar.tsx
@@ -1,49 +1,55 @@
-import { Avatar as BaseAvatar } from "@arco-design/web-react";
-import { ComponentImpl, implementRuntimeComponent } from "@sunmao-ui/runtime";
-import { css } from "@emotion/css";
-import { Type, Static } from "@sinclair/typebox";
-import { FALLBACK_METADATA, getComponentProps } from "../sunmao-helper";
-import { AvatarPropsSchema as BaseAvatarPropsSchema } from "../generated/types/Avatar";
+import { Avatar as BaseAvatar } from '@arco-design/web-react';
+import { ComponentImpl, implementRuntimeComponent } from '@sunmao-ui/runtime';
+import { css } from '@emotion/css';
+import { Type, Static } from '@sinclair/typebox';
+import { FALLBACK_METADATA, getComponentProps } from '../sunmao-helper';
+import { AvatarPropsSchema as BaseAvatarPropsSchema } from '../generated/types/Avatar';
 
 const AvatarPropsSchema = Type.Object({
   ...BaseAvatarPropsSchema,
 });
 const AvatarStateSchema = Type.Object({});
 
-const AvatarImpl: ComponentImpl<Static<typeof AvatarPropsSchema>> = (props) => {
-  const { elementRef, slotsElements, customStyle } = props;
-  const { ...cProps } = getComponentProps(props);
+const AvatarImpl: ComponentImpl<Static<typeof AvatarPropsSchema>> = props => {
+  const { slotsElements, elementRef, customStyle } = props;
+  const { type, src, text, ...cProps } = getComponentProps(props);
 
   return (
-    <BaseAvatar ref={elementRef} className={css(customStyle?.content)} {...cProps}>
-      {slotsElements.content}
+    <BaseAvatar
+      ref={elementRef}
+      className={css(customStyle?.content)}
+      {...cProps}
+      triggerIcon={slotsElements.triggerIcon}
+    >
+      {type === 'img' ? <img src={src} /> : text}
     </BaseAvatar>
   );
 };
 
 const exampleProperties: Static<typeof AvatarPropsSchema> = {
-  shape: "circle",
-  triggerType: "button",
-  size:50
+  shape: 'circle',
+  triggerType: 'button',
+  size: 50,
+  type: 'text',
 };
 
 const options = {
-  version: "arco/v1",
+  version: 'arco/v1',
   metadata: {
     ...FALLBACK_METADATA,
-    name: "avatar",
-    displayName: "Avatar",
+    name: 'avatar',
+    displayName: 'Avatar',
     exampleProperties,
     annotations: {
-      category: "Display",
-    }
+      category: 'Display',
+    },
   },
   spec: {
     properties: AvatarPropsSchema,
     state: AvatarStateSchema,
     methods: {},
-    slots: ["content"],
-    styleSlots: ["content"],
+    slots: ['triggerIcon'],
+    styleSlots: ['content'],
     events: [],
   },
 };
