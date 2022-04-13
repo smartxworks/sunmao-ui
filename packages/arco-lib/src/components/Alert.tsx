d17f76119e35f50e626a43cf741d22b30cd0e0a4
xzdry <dry@justdry.net>
2022-04-13 16:34:50 +0800

    refactor: refactor the alert component and add events to the alert and avatar components

--- packages/arco-lib/src/components/Alert.tsx
+++ packages/arco-lib/src/components/Alert.tsx
@@ -1,70 +1,61 @@
-import { Alert as BaseAlert } from "@arco-design/web-react";
-import { ComponentImpl, implementRuntimeComponent } from "@sunmao-ui/runtime";
-import { css } from "@emotion/css";
-import { Type, Static } from "@sinclair/typebox";
-import { FALLBACK_METADATA, getComponentProps } from "../sunmao-helper";
-import { AlertPropsSpec as BaseAlertPropsSpec } from "../generated/types/Alert";
+import { Alert as BaseAlert } from '@arco-design/web-react';
+import { ComponentImpl, implementRuntimeComponent } from '@sunmao-ui/runtime';
+import { css } from '@emotion/css';
+import { Type, Static } from '@sinclair/typebox';
+import { FALLBACK_METADATA, getComponentProps } from '../sunmao-helper';
+import { AlertPropsSpec as BaseAlertPropsSpec } from '../generated/types/Alert';
 
 const AlertPropsSpec = Type.Object(BaseAlertPropsSpec);
 const AlertStateSpec = Type.Object({});
 
-const AlertImpl: ComponentImpl<Static<typeof AlertPropsSpec>> = (props) => {
-  const { visible, content, title, elementRef, ...cProps } =
-    getComponentProps(props);
-  const { customStyle, slotsElements } = props;
+const AlertImpl: ComponentImpl<Static<typeof AlertPropsSpec>> = props => {
+  const { elementRef, ...cProps } = getComponentProps(props);
+  const { customStyle, slotsElements, callbackMap } = props;
 
-  return visible ? (
+  return (
     <BaseAlert
       ref={elementRef}
       action={slotsElements.action}
       icon={slotsElements.icon}
-      content={
-        <>
-          {content}
-          {slotsElements.content}
-        </>
-      }
-      title={
-        <>
-          {title}
-          {slotsElements.title}
-        </>
-      }
+      onClose={_e => {
+        callbackMap?.onClose?.();
+      }}
+      afterClose={()=>{
+        callbackMap?.afterClose?.();
+      }}
       className={css(customStyle?.content)}
       {...cProps}
     />
-  ) : null;
+  );
 };
 
 const exampleProperties: Static<typeof AlertPropsSpec> = {
-  disabled: false,
   closable: true,
-  title: "info",
-  content: "Here is an example text",
-  visible: true,
+  title: 'info',
+  content: 'Here is an example text',
   showIcon: true,
   banner: false,
-  type: "info",
+  type: 'info',
 };
 
 const options = {
-  version: "arco/v1",
+  version: 'arco/v1',
   metadata: {
     ...FALLBACK_METADATA,
-    name: "alert",
-    displayName: "Alert",
+    name: 'alert',
+    displayName: 'Alert',
     exampleProperties,
     annotations: {
-      category: "Display",
+      category: 'Display',
     },
   },
   spec: {
     properties: AlertPropsSpec,
     state: AlertStateSpec,
     methods: {},
-    slots: ["content", "action", "icon", "title"],
-    styleSlots: ["content"],
-    events: [],
+    slots: ['action', 'icon'],
+    styleSlots: ['content'],
+    events: ['onClose', 'afterClose'],
   },
 };
 
