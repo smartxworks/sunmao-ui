0da6af815af9647089f6bde0b09375e5d9069f7a
MrWindlike <MrWindlike@gmail.com>
2022-05-06 15:18:10 +0800

    chore: make the data type datasource informations as constants

--- packages/editor/src/constants/dataSource.ts
+++ packages/editor/src/constants/dataSource.ts
@@ -18,3 +18,24 @@ export const DATASOURCE_TRAIT_TYPE_MAP = {
   [DataSourceType.LOCALSTORAGE]: 'core/v1/localStorage',
   [DataSourceType.TRANSFORMER]: 'core/v1/transformer',
 };
+
+export const DATA_DATASOURCES = [
+  {
+    type: DataSourceType.STATE,
+    traitType: DATASOURCE_TRAIT_TYPE_MAP[DataSourceType.STATE],
+    filterPlaceholder: 'filter the states',
+    emptyPlaceholder: 'No States.',
+  },
+  {
+    type: DataSourceType.LOCALSTORAGE,
+    traitType: DATASOURCE_TRAIT_TYPE_MAP[DataSourceType.LOCALSTORAGE],
+    filterPlaceholder: 'filter the localStorages',
+    emptyPlaceholder: 'No LocalStorages.',
+  },
+  {
+    type: DataSourceType.TRANSFORMER,
+    traitType: DATASOURCE_TRAIT_TYPE_MAP[DataSourceType.TRANSFORMER],
+    filterPlaceholder: 'filter the transformers',
+    emptyPlaceholder: 'No Transformers.',
+  },
+];
