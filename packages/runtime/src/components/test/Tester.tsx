e18d3120b8105fd14e32b58b281e4b9ae359b76d
Bowen Tan <bowen.tan@smartx.com>
2022-06-08 17:09:14 +0800

    test(runtime): add single component test
    * main:
    feat: add the `requestFullscreen` method to the iframe component
    feat: add the iframe
    component

--- packages/runtime/src/components/test/Tester.tsx
+++ packages/runtime/src/components/test/Tester.tsx
@@ -2,7 +2,10 @@ import { implementRuntimeComponent } from '../../utils/buildKit';
 import { Type } from '@sinclair/typebox';
 import { useEffect } from 'react';
 
-let renderTimes = 1;
+(window as any).renderTimesMap = {};
+(window as any).destroyTimesMap = {};
+const renderTimesMap = (window as any).renderTimesMap;
+const destroyTimesMap = (window as any).destroyTimesMap;
 
 export default implementRuntimeComponent({
   version: 'test/v1',
@@ -30,13 +33,24 @@ export default implementRuntimeComponent({
     events: [],
   },
 })(({ testId, text }) => {
+  renderTimesMap[testId] = (renderTimesMap[testId] || 0) + 1;
+  console.log('render', renderTimesMap[testId]);
+
   useEffect(() => {
-    console.log('渲染了');
-    renderTimes++;
-  });
+    return () => {
+      destroyTimesMap[testId] = (destroyTimesMap[testId] || 0) + 1;
+    };
+  }, [testId]);
   return (
     <div>
-      <span data-testid={testId}>{renderTimes}</span>
+      <p>
+        <span>RenderTimes:</span>
+        <span data-testid={testId}>{renderTimesMap[testId] || 0}</span>{' '}
+      </p>
+      <p>
+        <span>DestroyTimes:</span>
+        <span data-testid={`${testId}-destroy`}>{destroyTimesMap[testId] || 0}</span>{' '}
+      </p>
       <span data-testid={`${testId}-text`}>{text}</span>
     </div>
   );
