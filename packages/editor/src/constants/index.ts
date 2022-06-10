67d4e49f5bb063a8645afcf169e821c165965e5c
MrWindlike <MrWindlike@gmail.com>
2022-06-10 09:58:35 +0800

    feat(editor): add the slot trait form
    ISSUES CLOSED: #440

--- packages/editor/src/constants/index.ts
+++ packages/editor/src/constants/index.ts
@@ -2,8 +2,9 @@ import { Application } from '@sunmao-ui/core';
 import { ImplementedRuntimeModule } from '@sunmao-ui/runtime';
 import { CORE_VERSION, CoreTraitName } from '@sunmao-ui/shared';
 
+export const unremovableTraits = [`${CORE_VERSION}/${CoreTraitName.Slot}`];
+
 export const ignoreTraitsList = [
-  `${CORE_VERSION}/${CoreTraitName.Slot}`,
   `${CORE_VERSION}/${CoreTraitName.Event}`,
   `${CORE_VERSION}/${CoreTraitName.Style}`,
   `${CORE_VERSION}/${CoreTraitName.Fetch}`,
--- packages/runtime/src/traits/core/Slot.tsx
+++ packages/runtime/src/traits/core/Slot.tsx
@@ -2,14 +2,18 @@ import { Type } from '@sinclair/typebox';
 import { CORE_VERSION, CoreTraitName } from '@sunmao-ui/shared';
 import { implementRuntimeTrait } from '../../utils/buildKit';
 
-const ContainerPropertySpec = Type.Object({
-  id: Type.String(),
-  slot: Type.String(),
-});
+const ContainerPropertySpec = Type.Object(
+  {
+    id: Type.String(),
+    slot: Type.String(),
+  },
+  // don't show this property in the editor
+  { conditions: [{ key: '', value: '' }] }
+);
 
 export const PropsSpec = Type.Object({
   container: ContainerPropertySpec,
-  ifCondition: Type.Optional(Type.Boolean()),
+  ifCondition: Type.Optional(Type.Boolean({})),
 });
 
 export default implementRuntimeTrait({
