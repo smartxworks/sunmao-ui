3a8f785fadd9046c278887d5c3d96731147d7e8a
tonggang <tonggang.double@bytedance.com>
2022-07-19 18:32:40 +0800

    fix: arco按需加载

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { virtualExamplePlugin } from '@sunmao-ui/vite-plugins';
 import { resolve } from 'path';
+import vitePluginForArco from '@arco-plugins/vite-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    virtualExamplePlugin(),
-  ],
+  plugins: [react(), virtualExamplePlugin(), vitePluginForArco({ style: 'css' })],
   define: {
     // https://github.com/satya164/react-simple-code-editor/issues/86
     global: 'globalThis',
