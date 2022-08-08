3e6d2649d44c6800c5b0eb76d90b2721e17a6748
xzdry <ruoyu.dong@smartx.com>
2022-08-08 09:46:09 +0800

    feat(LocalStorage): add expire and versions to localstorage

--- packages/runtime/src/traits/core/LocalStorage.tsx
+++ packages/runtime/src/traits/core/LocalStorage.tsx
@@ -2,17 +2,90 @@ import { Type } from '@sinclair/typebox';
 import { implementRuntimeTrait } from '../../utils/buildKit';
 import { CORE_VERSION, CoreTraitName } from '@sunmao-ui/shared';
 
-function getLocalStorageValue(key: string) {
+const isEmpty = (value: unknown) => value === '' || value === null || value === undefined;
+
+function setLocalStorage(key: string, value: unknown, options: LocalStorageItem['meta']) {
+  const { versions = 0, expire = 0 } = options;
+  if (isEmpty(value)) value = null;
+
+  if (isNaN(expire) || isNaN(versions)) {
+    throw new Error('Expire or Version must be number');
+  }
+
+  const data = {
+    value,
+    meta: {
+      time: Date.now(),
+      versions,
+      ...(expire === 0 ? {} : { expire }),
+    },
+  };
+
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+function removeStorage(key: string) {
+  localStorage.removeItem(key);
+}
+
+// 1min
+const DEFAULT_TIME_INTERVAL = 60 * 1000;
+
+function getLocalStorage(
+  key: string,
+  defaultValue = null,
+  options: LocalStorageItem['meta']
+) {
+  const { versions = 0 } = options;
   try {
-    const json = localStorage.getItem(key);
-    if (json) {
-      return JSON.parse(json);
+    const value = localStorage.getItem(key);
+
+    if (!value) {
+      return null;
     }
-    return null;
+
+    let localItem;
+    try {
+      localItem = JSON.parse(value) as LocalStorageItem;
+    } catch {
+      // ignore
+    }
+
+    if (!localItem?.meta?.versions || versions > localItem?.meta?.versions) {
+      setLocalStorage(key, defaultValue, { versions });
+      return defaultValue;
+    }
+
+    const now = Date.now();
+    if (localItem.meta.expire) {
+      const expire = localItem.meta.expire * DEFAULT_TIME_INTERVAL;
+
+      if (expire < now - (localItem.meta.time ?? now)) {
+        removeStorage(key);
+        return null;
+      } else {
+        setLocalStorage(key, localItem.value, {
+          versions,
+          expire: localItem.meta.expire,
+        });
+        return localItem.value;
+      }
+    } else {
+      return localItem.value;
+    }
   } catch (error) {
     return null;
   }
 }
+
+type LocalStorageItem = {
+  value: any;
+  meta: {
+    time?: number;
+    versions?: number;
+    expire?: number;
+  };
+};
 
 export const LocalStorageTraitPropertiesSpec = Type.Object({
   key: Type.String({
@@ -20,6 +93,12 @@ export const LocalStorageTraitPropertiesSpec = Type.Object({
   }),
   initialValue: Type.Any({
     title: 'Initial Value',
+  }),
+  versions: Type.Number({
+    title: 'Versions',
+  }),
+  expire: Type.Number({
+    title: 'Expire',
   }),
 });
 
@@ -44,7 +123,15 @@ export default implementRuntimeTrait({
 })(() => {
   const HasInitializedMap = new Map<string, boolean>();
 
-  return ({ key, initialValue, componentId, mergeState, subscribeMethods }) => {
+  return ({
+    key,
+    versions,
+    initialValue,
+    expire,
+    componentId,
+    mergeState,
+    subscribeMethods,
+  }) => {
     const hashId = `#${componentId}@${key}`;
     const hasInitialized = HasInitializedMap.get(hashId);
 
@@ -52,12 +139,12 @@ export default implementRuntimeTrait({
       mergeState({
         [key]: newValue,
       });
-      localStorage.setItem(hashId, JSON.stringify(newValue));
+      setLocalStorage(hashId, newValue, { versions, expire });
     };
 
     if (key) {
       if (!hasInitialized) {
-        const value = getLocalStorageValue(hashId) ?? initialValue;
+        const value = getLocalStorage(hashId, initialValue, { versions, expire });
         setValue(value);
 
         subscribeMethods({
