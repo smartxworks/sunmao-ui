789058e1e37f5d5ac93e1ae2647817f6d26edfc8
Bowen Tan <bowen.tan@smartx.com>
2022-08-15 18:16:45 +0800

    refactor(runtime): remove full import of lodash

--- packages/shared/src/utils/object.ts
+++ packages/shared/src/utils/object.ts
@@ -1,11 +1,9 @@
-import { isObject, isArray } from 'lodash';
-
 export function traverse(value: unknown, seen: Set<unknown> = new Set()) {
-  if (!isObject(value) || seen.has(value)) return value;
+  if (typeof value !== 'object' || seen.has(value)) return value;
 
   seen.add(value);
 
-  if (isArray(value)) {
+  if (Array.isArray(value)) {
     for (let i = 0; i < value.length; i++) traverse(value[i], seen);
   } else if (value instanceof Map) {
     value.forEach((_, key) => {
@@ -16,7 +14,7 @@ export function traverse(value: unknown, seen: Set<unknown> = new Set()) {
     value.forEach(v => {
       traverse(v, seen);
     });
-  } else {
+  } else if (value) {
     for (const key of Object.keys(value))
       traverse(value[key as keyof typeof value], seen);
   }
