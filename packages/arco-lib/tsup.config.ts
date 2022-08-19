b2fb3940318c98d0b9255fe76fb35b72d789af56
Bowen Tan <bowen.tan@smartx.com>
2022-08-19 11:47:13 +0800

    build(dyamic-import): make some editor files dynamic import

--- packages/arco-lib/tsup.config.ts
+++ packages/arco-lib/tsup.config.ts
@@ -3,9 +3,10 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/lib.ts', 'src/index.ts', 'src/widgets/index.ts'],
   format: ['cjs', 'esm', 'iife'],
-  inject: ['./react-import.js'],
   splitting: true,
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: false,
+  metafile: true,
+  platform: 'browser',
 });
